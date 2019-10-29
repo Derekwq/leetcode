54d5046aad97887b8bdfc447524361054995413c
Derek Wu <derek.wu@aliyun.com>
2019-10-30 00:06:38 +0800

    feat: 完成Problem 222: 完全二叉树的节点个数

--- src/main/java/com/derek/leetcode/ds/InvertBinaryTree.java
+++ src/main/java/com/derek/leetcode/ds/InvertBinaryTree.java
@@ -37,39 +37,4 @@ public class InvertBinaryTree {
         return new TreeNode(node.val);
     }
 
-    public static class TreeNode {
-
-        int      val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
-        }
-
-        public static TreeNode build(int[] arr) {
-            if (arr == null || arr.length == 0) {
-                return null;
-            }
-            TreeNode root = new TreeNode(arr[0]);
-            fillChildren(root, 0, arr);
-            return root;
-        }
-
-        private static void fillChildren(TreeNode curNode, int curIdx, int[] arr) {
-            TreeNode left = null;
-            TreeNode right = null;
-            if (2 * curIdx + 1 < arr.length) {
-                left = new TreeNode(arr[2 * curIdx + 1]);
-                fillChildren(left, 2 * curIdx + 1, arr);
-            }
-            if (2 * curIdx + 2 < arr.length) {
-                right = new TreeNode(arr[2 * curIdx + 2]);
-                fillChildren(right, 2 * curIdx + 2, arr);
-            }
-            curNode.left = left;
-            curNode.right = right;
-        }
-    }
-
 }
