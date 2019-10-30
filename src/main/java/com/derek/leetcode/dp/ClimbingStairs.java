4f716283f4480ffc2f0d4cd2efd27a6d6c596231
Derek Wu <derek.wu@aliyun.com>
2019-10-30 23:37:37 +0800

    DP, 70: 爬楼梯

---
+++ src/main/java/com/derek/leetcode/dp/ClimbingStairs.java
@@ -0,0 +1,31 @@
+package com.derek.leetcode.dp;
+
+/**
+ * 70. 爬楼梯
+ * https://leetcode-cn.com/problems/climbing-stairs/
+ * 
+ * @author derek.wu
+ * @date 2019-10-30
+ * @since v1.0.0
+ */
+public class ClimbingStairs {
+}
+
+class Solution {
+
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int dp[] = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
+    }
+}
