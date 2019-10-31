85e5a887b7f6b05a380604f0907017860d03f1ed
Derek Wu <derek.wu@aliyun.com>
2019-10-31 17:12:56 +0800

    DP, 96: 不同的二叉搜索树

--- src/main/java/com/derek/leetcode/dp/DecodeWays.java
+++ src/main/java/com/derek/leetcode/dp/DecodeWays.java
@@ -25,31 +25,30 @@ public class DecodeWays {
         System.out.println("s: " + s);
         System.out.println("ans: " + solution.numDecodings(s));
     }
-}
 
-class Solution {
+    static class Solution {
 
-    public int numDecodings(String s) {
-        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
-            return 0;
-        }
-
-        int pre = 1;
-        int cur = 1;
-        for (int i = 1; i < s.length(); i++) {
-            int tmp = cur;
-            if (s.charAt(i) == '0') {
-                if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2') {
-                    cur = pre;
-                } else {
-                    return 0;
-                }
-            } else if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2' && s.charAt(i) <= '6') {
-                cur += pre;
+        public int numDecodings(String s) {
+            if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+                return 0;
             }
-            pre = tmp;
-        }
-        return cur;
-    }
 
+            int pre = 1;
+            int cur = 1;
+            for (int i = 1; i < s.length(); i++) {
+                int tmp = cur;
+                if (s.charAt(i) == '0') {
+                    if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2') {
+                        cur = pre;
+                    } else {
+                        return 0;
+                    }
+                } else if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2' && s.charAt(i) <= '6') {
+                    cur += pre;
+                }
+                pre = tmp;
+            }
+            return cur;
+        }
+    }
 }
