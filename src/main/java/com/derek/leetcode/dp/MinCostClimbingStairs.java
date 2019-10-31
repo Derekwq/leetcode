61d3a68b935ca80cc3487ff01674d3575eebd5eb
Derek Wu <derek.wu@aliyun.com>
2019-10-31 10:31:37 +0800

    DP, 1025: 除数博弈

--- src/main/java/com/derek/leetcode/dp/MinCostClimbingStairs.java
+++ src/main/java/com/derek/leetcode/dp/MinCostClimbingStairs.java
@@ -22,23 +22,22 @@ public class MinCostClimbingStairs extends Tool {
         System.out.println(printArr(cost) + " ==> " + solution.minCostClimbingStairs(cost));
     }
 
-}
+    static class Solution {
 
-class Solution {
-
-    public int minCostClimbingStairs(int[] cost) {
-        if (cost == null || cost.length == 0) {
-            return 0;
+        public int minCostClimbingStairs(int[] cost) {
+            if (cost == null || cost.length == 0) {
+                return 0;
+            }
+            if (cost.length == 1) {
+                return cost[0];
+            }
+            int[] dp = new int[cost.length];
+            dp[0] = cost[0];
+            dp[1] = cost[1];
+            for (int i = 2; i < cost.length; i++) {
+                dp[i] = Math.min(dp[i - 2] + cost[i], dp[i - 1] + cost[i]);
+            }
+            return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
         }
-        if (cost.length == 1) {
-            return cost[0];
-        }
-        int[] dp = new int[cost.length];
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-        for (int i = 2; i < cost.length; i++) {
-            dp[i] = Math.min(dp[i - 2] + cost[i], dp[i - 1] + cost[i]);
-        }
-        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
     }
 }
