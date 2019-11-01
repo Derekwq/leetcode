914e6e8385952189d996d9addae423ac6f0a67d2
Derek Wu <derek.wu@aliyun.com>
2019-11-01 11:56:24 +0800

    Tree, DFS, 101: 对称二叉树

--- src/main/java/com/derek/leetcode/dfs/SameTree.java
+++ src/main/java/com/derek/leetcode/dfs/SameTree.java
@@ -12,20 +12,19 @@ import com.derek.leetcode.TreeNode;
  */
 public class SameTree {
 
-}
+    static class Solution {
 
-class Solution {
-
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
-            return true;
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == null && q == null) {
+                return true;
+            }
+            if (p == null || q == null) {
+                return false;
+            }
+            if (p.val != q.val) {
+                return false;
+            }
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
-        if (p == null || q == null) {
-            return false;
-        }
-        if (p.val != q.val) {
-            return false;
-        }
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
