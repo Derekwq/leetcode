caf7cd7fe864490b21637ef3f37cdc0370f63e82
Derek Wu <derek.wu@aliyun.com>
2019-11-01 11:59:31 +0800

    Tree, 104: 二叉树的最大深度

---
+++ src/main/java/com/derek/leetcode/dfs/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,24 @@
+package com.derek.leetcode.dfs;
+
+import com.derek.leetcode.TreeNode;
+
+/**
+ * 104. 二叉树的最大深度
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ * 
+ * @author derek.wu
+ * @date 2019-11-01
+ * @since v1.0.0
+ */
+public class MaximumDepthOfBinaryTree {
+}
+
+class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
